66dc4dea353b7d681b187c554188ff142f9c463a
ambArmando <armando.ambrus@student.upt.ro>
2021-10-09 15:12:35 +0300

    L02restAPI

---
+++ AmbrusArmando/L02/L02ambArmando/datcL02_RESTAPI/Models/Student.cs
@@ -0,0 +1,16 @@
+namespace StudentsAPI.Models {
+
+    public class Student {
+
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    
+        public int YearOfStudy { get; set; }
+    }
+
+}
+
+
