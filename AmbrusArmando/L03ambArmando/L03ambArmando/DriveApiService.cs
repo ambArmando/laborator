4ea6554389e8129a738fb1f8cf9e88accf83b537
ambArmando <armando.ambrus@student.upt.ro>
2021-10-25 21:09:28 +0300

    tema 3

---
+++ AmbrusArmando/L03ambArmando/L03ambArmando/DriveApiService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace L03ambArmando
+{
+    public class DriveApiService
+    {
+        protected static string[] scopes = { DriveService.Scope.Drive };
+        protected readonly UserCredential credential;
+        static string ApplicationName = "ambAPIs";
+        protected readonly DriveService service;
+        protected readonly FileExtensionContentTypeProvider fileExtensionProvider;
+        private const string gmail = "armando.ambrus@gmail.com";
+
+        public DriveApiService()
+        {
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    gmail, // use a const or read it from a config file
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+
+                fileExtensionProvider = new FileExtensionContentTypeProvider();
+            }
+
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        public IList<Google.Apis.Drive.v3.Data.File> ListEntities(string id = "root")
+        {
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 100;
+            listRequest.Fields = "nextPageToken, files(id, name, parents, createdTime, modifiedTime, mimeType)";
+            listRequest.Q = $"'{id}' in parents";
+
+            return listRequest.Execute().Files;
+        }
+
+        public async Task<Google.Apis.Drive.v3.Data.File> Upload(IFormFile file, string documentId)
+        {
+            var name = ($"{DateTime.UtcNow.ToString()}.{Path.GetExtension(file.FileName)}");
+            var mimeType = file.ContentType;
+
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = name,
+                MimeType = mimeType,
+                Parents = new[] { documentId }
+            };
+
+            FilesResource.CreateMediaUpload request;
+            using (var stream = file.OpenReadStream())
+            {
+                request = service.Files.Create(
+                    fileMetadata, stream, mimeType);
+                request.Fields = "id, name, parents, createdTime, modifiedTime, mimeType, thumbnailLink";
+                await request.UploadAsync();
+            }
+
+            return request.ResponseBody;
+        }
+    }
+}
---
+++ AmbrusArmando/L03ambArmando/L03ambArmando/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace L03ambArmando
+{
+    class Program
+    {
+        private static DriveApiService driveApiService = new DriveApiService();
+        private static IList<Google.Apis.Drive.v3.Data.File> filesList;
+
+        private static void listFolders()
+        {
+            filesList = driveApiService.ListEntities();
+            foreach (var file in filesList)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+        }
+
+        private static void uploadFile() {
+            
+        
+        }
+
+        static void Main(string[] args)
+        {
+            listFolders();
+
+            Console.ReadKey();
+        }
+    }
+}
